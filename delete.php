90a47d6d9e62838e8935e32db3154a735afeee6a
Niaz Bin Siraj <niaz9767@gmail.com>
2022-01-06 00:56:54 +0600

    Project Completed

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $data = file_get_contents('data.json');
+    $students = json_decode($data, true);
+
+    foreach ($students as $i => $student) {
+        if ($student['id'] == $id) {
+            unset($students[$i]);
+            $save = json_encode(array_values($students), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            file_put_contents('data.json', $save);
+            break;
+        }
+    }
+    header('Location: index.php');
+}
+die();
+
+?>
